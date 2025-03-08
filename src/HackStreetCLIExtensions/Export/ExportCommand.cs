3a8b0ca6f9a87711d8defe2d3b919a55f8464724
ChiragKhanna <ckhanna@horizontal.com>
2025-03-08 10:25:56 +0530

    initial commit for cli

---
+++ src/HackStreetCLIExtensions/Export/ExportCommand.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Options;
+using Sitecore.CH.Cli.Core.Abstractions.Commands;
+using Sitecore.CH.Cli.Core.Abstractions.Rendering;
+using SitecoreCHCLIExtensions.Models;
+using Stylelabs.M.Sdk.WebClient;
+using System.CommandLine.Invocation;
+
+namespace SitecoreCHCLIExtensions.Export
+{
+    public class ExportCommand : BaseCommand<ExportCommandHandler>
+    {
+        public ExportCommand() : base("export", "Export Assets")
+        {
+            AddOption<string>(ExportMessages.ExportCommandQuery, false, new string[2]
+            {
+                "--query",
+                "-q"
+            });
+            AddOption<string>(ExportMessages.ExportCommandLocation, false, new string[2]
+            {
+                "--location",
+                "-l"
+            });
+            AddOption<string>(ExportMessages.ExportCommandFields, false, new string[2]
+            {
+                "--fields",
+                "-f"
+            });
+        }
+    }
+    public class ExportCommandHandler : BaseCommandHandler
+    {
+        public ExportCommandHandler(Lazy<IWebMClient> client, IOutputRenderer renderer, IOptions<ExportParameters> parameters) : base(client, renderer)
+        {
+            this.Parameters = parameters?.Value;
+        }
+        public ExportParameters Parameters { get; set; }
+
+        public override Task<int> InvokeAsync(InvocationContext context)
+        {
+            Renderer.WriteLine(this.Parameters.Query);
+            Renderer.WriteLine(this.Parameters.Fields);
+            Renderer.WriteLine(this.Parameters.Location);
+            // Return exit code to indicate success or failure
+            return Task.FromResult(0);
+        }
+    }
+}
---
+++ src/HackStreetCLIExtensions/Plugins/HackStreetPlugin.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sitecore.CH.Cli.Core.Abstractions.Infrastructure;
+using Sitecore.CH.Cli.Core.Extensions;
+using SitecoreCHCLIExtensions.Export;
+using SitecoreCHCLIExtensions.Models;
+using System.CommandLine;
+
+namespace SitecoreCHCLIExtensions.Plugins
+{
+    public class HackStreetPlugin : IPlugin
+    {
+        //This is required to register the command handler
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCommandHandler<ExportCommandHandler, ExportParameters>();
+        }
+
+        public void RegisterCommands(ICommandRegistry registry)
+        {
+            registry.RegisterCommandGroup(
+     "hackstreet", new List<Command> { new ExportCommand() }, "Hackstreet plugin for cli extension");
+        }
+    }
+}
